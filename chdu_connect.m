9a73e30b4b051a2e11370e6ed850655b25e59034
TexnoMan <antipov.v.a@yandex.ru>
2022-06-02 20:58:06 +0300

    Add launcher updating

--- chdu_connect.m
+++ chdu_connect.m
@@ -11,16 +11,21 @@ function chdu = chdu_connect()
     try
         connect_options = weboptions('ContentType', 'auto', ...
                'CharacterEncoding', 'UTF-8');
+
+        launcher_version_response = webread(strcat('http://hdu.vedyakov.com:5000','/matlab_launcher_version'), connect_options);
+        new_hash = launcher_version_response.data.md5;
+        check_hash = strcmp(new_hash, current_hash);
+        if ~check_hash
+            websave('chdu_connect.m', 'https://raw.githubusercontent.com/ITMORobotics/hwc-matlab-client/main/chdu_connect.m')
+            disp("Сient has been updated")
+        end
+
         version_response = webread(strcat('http://hdu.vedyakov.com:5000','/matlab_client_version'), connect_options);
-        if version_response.isError
-            return
-        else
-            new_hash = version_response.data.md5;
-            check_hash = strcmp(new_hash, current_hash);
-            if ~check_hash
-                websave('CHDU.m', 'https://raw.githubusercontent.com/ITMORobotics/hwc-matlab-client/main/CHDU.m')
-                disp("Сient has been updated")
-            end
+        new_hash = version_response.data.md5;
+        check_hash = strcmp(new_hash, current_hash);
+        if ~check_hash
+            websave('CHDU.m', 'https://raw.githubusercontent.com/ITMORobotics/hwc-matlab-client/main/CHDU.m')
+            disp("Сient has been updated")
         end
     catch
         disp('Can not get client version... Please try later')
