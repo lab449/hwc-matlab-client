039a0ca85760933a828192ca0807cd2779a1836f
TexnoMan <antipov.v.a@yandex.ru>
2022-06-05 11:50:20 +0300

    Update launcher

--- chdu_connect.m
+++ chdu_connect.m
@@ -13,13 +13,13 @@ function chdu = chdu_connect()
         connect_options = weboptions('ContentType', 'auto', ...
                'CharacterEncoding', 'UTF-8');
 
-        websave('chdu_connect.m', 'https://raw.githubusercontent.com/ITMORobotics/hwc-matlab-client/main/chdu_connect.m');
+        websave('chdu_connect.m', 'https://hdu.vedyakov.com/files/hwc-matlab-client/chdu_connect.m');
 
         version_response = webread(strcat('https://hdu.vedyakov.com','/api/matlab_client_version'), connect_options);
         new_hash = version_response.data.md5;
         check_hash = strcmp(new_hash, current_hash);
         if ~check_hash
-            websave('CHDU.m', 'https://raw.githubusercontent.com/ITMORobotics/hwc-matlab-client/main/CHDU.m')
+            websave('CHDU.m', 'https://hdu.vedyakov.com/files/hwc-matlab-client/CHDU.m')
             disp("Сient has been updated")
         end
     catch
