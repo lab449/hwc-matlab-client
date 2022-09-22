677e1483c16096e7355f3e3cbdcf928a0503c66b
TexnoMann <ElectroDans@yandex.ru>
2022-09-22 15:28:49 +0300

    Update client and launcher updating

--- hwc_connect.m
+++ hwc_connect.m
@@ -12,23 +12,23 @@ function hwc = hwc_connect(server)
     char(bi.toString(16));
     current_hash = char(String.format('%032x', bi));
     
-    try
+%     try
         connect_options = weboptions('ContentType', 'auto', ...
                'CharacterEncoding', 'UTF-8');
 
-        websave('hwc_connect.m', strcat(server, '/files/hwc-matlab-client/hwc_connect.m'));
+        websave('hwc_connect.m', strcat(server, '/clints/hwc-matlab-client/hwc_connect.m'));
 
         version_response = webread(strcat(server,'/api/matlab/client_version'), connect_options);
         new_hash = version_response.data.md5;
         check_hash = strcmp(new_hash, current_hash);
         if ~check_hash
-            websave('HWC.m', strcat(server, '/files/hwc-matlab-client/HWC.m'));
+            websave('HWC.m', strcat(server, '/clients/hwc-matlab-client/HWC.m'));
             disp("Сient has been updated")
         end
-    catch
-        disp('Can not get client version... Please try later')
-        return
-    end
+%     catch
+%         disp('Can not get client version... Please try later')
+%         return
+%     end
     hwc = nan;
     hwc = HWC(server);
     ok = hwc.login();
